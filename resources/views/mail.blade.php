c12334c263d403f8927ad1a6992e776ae4ba52b5
RafifulHan <anang2018rplrus2@gmail.com>
2020-01-16 14:05:11 +0700

    Weastore Update1

---
+++ resources/views/mail.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Message</title>
+</head>
+<body>
+
+        <h4><b>Message: </b></h4><p>{{ $msg }}</p>
+
+</body>
+</html>
